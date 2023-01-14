75fbffa87d57e188149abde0a2dfce80d0a050bb
lucaslins0035 <lucaslinssouza@gmail.com>
2023-01-14 16:34:00 -0300

    Update Screens

--- main.py
+++ main.py
@@ -2,22 +2,26 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.label import Label
 from kivy.metrics import dp
-from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 
 
 class PlayMenu(Screen):
-    pass
+    def on_create_room(self):
+        self.manager.current = 'lobby'
+
+    def on_enter_room():
+        pass
 
 
 class Lobby(Screen):
+    # Create graphics
     def __init__(self, **kw):
         super().__init__(**kw)
-        anchor1 = AnchorLayout()
+        box_background = BoxLayout(orientation='vertical')
 
-        box1 = BoxLayout(orientation="vertical",
-                         size_hint=(0.4, 0.7))
+        players_grid = GridLayout(rows=5)
 
         # Lobby list
         title = Label(text="Players",
@@ -27,30 +31,34 @@ class Lobby(Screen):
                       size_hint=(1, None),
                       size=(dp(100), dp(80)))
         title.text_size = title.size
-        box1.add_widget(title)
 
         self.players_list = []
-        for i in range(6):
+        for i in range(10):
             self.players_list.append(
-                Label(text="{}: ***********".format("H" if i==0 else str(i+1)),
-                      font_size=20,
-                    #   halign="left",
-                    #   valign="center",
-                      size_hint=(1, None),
-                      size=("200dp", dp(50))))
-            # self.players_list[-1].text_size = self.players_list[-1].size
-            box1.add_widget(self.players_list[-1])
+                Label(text="{}: ***********".format("H" if i == 0 else str(i+1)),
+                      font_size=20)
+            )
+            players_grid.add_widget(self.players_list[-1])
 
-        anchor1.add_widget(box1)
-        self.add_widget(anchor1)
+        btns_box = BoxLayout(orientation="horizontal",
+                             size_hint=(1, None), size=(dp(100), dp(100)))
+        btns_box.add_widget(Button(text='Leave'))
+        btns_box.add_widget(Button(text='Play'))
 
+        box_background.add_widget(title)
+        box_background.add_widget(players_grid)
+        box_background.add_widget(btns_box)
 
-class WindowManager(ScreenManager):
-    pass
+        self.add_widget(box_background)
 
 
 class UpAndDownApp(App):
-    pass
+    def build(self):
+        # Create the screen manager
+        sm = ScreenManager()
+        sm.add_widget(PlayMenu(name="play_menu"))
+        sm.add_widget(Lobby(name="lobby"))
+        return sm
 
 
 UpAndDownApp().run()
