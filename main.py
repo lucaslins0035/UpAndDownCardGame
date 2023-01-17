a9f3e75ea32cce93a30c941aabe13fc5253df2fe
lucaslins0035 <lucaslinssouza@gmail.com>
2023-01-17 14:13:44 -0300

    Update play menu with kivyMD

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import threading
 
-from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.label import Label
+mobile_screen_ration = 19.5/9
+from kivy.config import Config
+Config.set('graphics', 'width', '498')
+Config.set('graphics', 'height', '1080')
+
+from kivymd.app import MDApp
+from kivymd.uix.screenmanager import MDScreenManager
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.label import MDLabel
+from kivymd.uix.gridlayout import MDGridLayout
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.button import MDTextButton
 from kivy.metrics import dp
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.properties import StringProperty, Clock, ListProperty
+from kivy.properties import StringProperty, Clock
+
 
 from gameServer import GameServer
 from gameclient import GameClient
@@ -17,22 +24,24 @@ game_server = GameServer('127.0.0.1', 7777)
 game_client = GameClient('127.0.0.1', 7777)
 
 
-class PlayMenu(Screen):
+class PlayMenu(MDScreen):
     warning_text = StringProperty("")
 
-    def on_create_room(self, name_input, ip_input, port_input):
-        if name_input.text == "":
-            self.warning_text = "Please provide a name"
-        elif ip_input.text == "":
-            self.warning_text = "Please provide IP address"
-        elif port_input.text == "":
-            self.warning_text = "Please provide port"
+    def on_create_room(self):
+        if self.ids.name_input.text == "" or self.ids.name_input.error:
+            self.ids.name_input.error = True
+        elif self.ids.ip_input.text == "":
+            self.ids.ip_input.error = True
+        elif self.ids.port_input.text == "":
+            self.ids.port_input.error = True
         else:
             global game_server
-            game_server = GameServer(ip_input.text, int(port_input.text))
+            game_server = GameServer(
+                self.ids.ip_input.text, int(self.ids.port_input.text))
 
             global game_client
-            game_client = GameClient(ip_input.text, int(port_input.text))
+            game_client = GameClient(
+                self.ids.ip_input.text, int(self.ids.port_input.text))
             game_client.type = HOST
 
             global server_thread
@@ -47,29 +56,30 @@ class PlayMenu(Screen):
             server_thread.join(0.2)
 
             if server_thread.is_alive():
-                game_client.set_name(name_input.text)
+                game_client.set_name(self.ids.name_input.text)
                 client_thread.start()
                 self.manager.current = 'lobby'
             else:
                 self.warning_text = "Address already in use"
 
-    def on_enter_room(self, name_input, ip_input, port_input):
-        if name_input.text == "":
-            self.warning_text = "Please provide a name"
-        elif ip_input.text == "":
-            self.warning_text = "Please provide IP address"
-        elif port_input.text == "":
-            self.warning_text = "Please provide port"
+    def on_enter_room(self):
+        if self.ids.name_input.text == "" or self.ids.name_input.error:
+            self.ids.name_input.error = True
+        elif self.ids.ip_input.text == "":
+            self.ids.ip_input.error = True
+        elif self.ids.port_input.text == "":
+            self.ids.port_input.error = True
         else:
             global game_client
-            game_client = GameClient(ip_input.text, int(port_input.text))
+            game_client = GameClient(
+                self.ids.ip_input.text, int(self.ids.port_input.text))
             game_client.type = CLIENT
 
             global client_thread
             client_thread = threading.Thread(
                 target=game_client.run, daemon=True)
 
-            game_client.set_name(name_input.text)
+            game_client.set_name(self.ids.name_input.text)
             client_thread.start()
             client_thread.join(0.2)
 
@@ -79,41 +89,42 @@ class PlayMenu(Screen):
                 self.warning_text = "There is no server running with this address"
 
 
-class Lobby(Screen):
+class Lobby(MDScreen):
     # Create graphics
     def __init__(self, **kw):
         super().__init__(**kw)
-        box_background = BoxLayout(orientation='vertical')
+        box_background = MDBoxLayout(orientation='vertical')
 
-        players_grid = GridLayout(rows=5)
+        players_grid = MDGridLayout(rows=5)
 
         # Lobby list
-        title = Label(text="Players",
-                      font_size=30,
-                      halign="center",
-                      valign="center",
-                      size_hint=(1, None),
-                      size=(dp(100), dp(80)))
+        title = MDLabel(text="Players",
+                        font_size=30,
+                        halign="center",
+                        valign="center",
+                        size_hint=(1, None),
+                        size=(dp(100), dp(80)))
         title.text_size = title.size
 
         self.players_list = []
         for i in range(10):
             self.players_list.append(
-                Label(text="{}: ***********".format("H" if i == 0 else str(i+1)),
-                      font_size=20)
+                MDLabel(text="{}: ***********".format("H" if i == 0 else str(i+1)),
+                        font_size=20)
             )
             players_grid.add_widget(self.players_list[-1])
 
-        btns_box = BoxLayout(orientation="horizontal",
-                             size_hint=(1, None), size=(dp(0), dp(60)),
-                             spacing=dp(25))
+        btns_box = MDBoxLayout(orientation="horizontal",
+                               size_hint=(1, None), size=(dp(0), dp(60)),
+                               spacing=dp(25),
+                               padding=dp(25))
 
-        self.leave_btn = Button(text='Leave',
-                                size_hint=(0.6, 1))
+        self.leave_btn = MDTextButton(text='Leave',
+                                      size_hint=(0.6, 1))
         self.leave_btn.bind(on_release=self.on_leave_btn)
 
-        self.play_btn = Button(text='Play',
-                               size_hint=(0.6, 1))
+        self.play_btn = MDTextButton(text='Play',
+                                     size_hint=(0.6, 1))
 
         btns_box.add_widget(self.leave_btn)
         btns_box.add_widget(self.play_btn)
@@ -187,22 +198,22 @@ class Lobby(Screen):
                 "H" if i == 0 else str(i+1), "")
 
 
-class UpAndDownApp(App):
+class UpAndDownApp(MDApp):
     def build(self):
         # Create the screen manager
-        sm = ScreenManager()
+        sm = MDScreenManager()
         sm.add_widget(PlayMenu(name="play_menu"))
         sm.add_widget(Lobby(name="lobby"))
         return sm
 
 
 # Main
-try:
-    UpAndDownApp().run()
-except:
-    if server_thread.is_alive():
-        game_server.close_server = True
-        server_thread.join()
-    if client_thread.is_alive():
-        game_client.close_client = True
-        client_thread.join()
+# try:
+UpAndDownApp().run()
+# except:
+#     if server_thread.is_alive():
+#         game_server.close_server = True
+#         server_thread.join()
+#     if client_thread.is_alive():
+#         game_client.close_client = True
+#         client_thread.join()
