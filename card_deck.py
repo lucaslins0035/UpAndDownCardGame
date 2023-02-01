05280e36393f443014e2229a4a02ad832793d8a2
lucaslins0035 <lucaslinssouza@gmail.com>
2023-02-01 17:33:21 -0300

    Add card and deck classes

---
+++ card_deck.py
@@ -0,0 +1,75 @@
+from pydealer.deck import Deck
+from pydealer.stack import Stack
+
+
+class CardDeck():
+    def __init__(self):
+        self.deck = Deck()
+        self.deck.shuffle()
+
+        # Up and down card rank
+        self.UD_RANK = {
+            "values": {
+                "Ace": 13,
+                "King": 12,
+                "Queen": 11,
+                "Jack": 10,
+                "10": 9,
+                "9": 8,
+                "8": 7,
+                "7": 6,
+                "6": 5,
+                "5": 4,
+                "4": 3,
+                "3": 2,
+                "2": 1,
+                "Joker": 0
+            },
+            "suits": {
+                "Spades": 4,
+                "Hearts": 3,
+                "Clubs": 2,
+                "Diamonds": 1
+            }
+        }
+
+    def update_card_rank(self, init, wild):
+        aux = 2
+        for suit in self.UD_RANK['suits'].keys():
+            if suit == wild:
+                self.UD_RANK['suits'][suit] = 4
+            elif suit == init:
+                self.UD_RANK['suits'][suit] = 3
+            else:
+                self.UD_RANK['suits'][suit] = aux
+                aux = aux-1
+
+    def gt(self, card_a, card_b):
+        if self.UD_RANK['suits'][card_a.suit] > self.UD_RANK['suits'][card_b.suit]:
+            return True
+        elif self.UD_RANK['suits'][card_a.suit] == self.UD_RANK['suits'][card_b.suit]:
+            if self.UD_RANK['values'][card_a.value] > self.UD_RANK['values'][card_b.value]:
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def get_max_card(self, stack):
+        return self.sort_cards(stack)[0]
+
+    def sort_cards(self, cards):
+        if self.UD_RANK.get("values"):
+            cards = sorted(
+                cards,
+                key=lambda x: self.UD_RANK["values"][x.value],
+                reverse=True
+            )
+        if self.UD_RANK.get("suits"):
+            cards = sorted(
+                cards,
+                key=lambda x: self.UD_RANK["suits"][x.suit],
+                reverse=True
+            )
+
+        return Stack(list=cards)
